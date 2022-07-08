791e0d060fd2bd7c5cbdee8d5aa6d4ee98a6f073
Wonuola Alonge <57100593+wonuola-y@users.noreply.github.com>
2022-07-08 18:27:47 +0100

    update
    made an update on the buttons

--- index.js
+++ index.js
@@ -5,12 +5,8 @@ let mainPage = document.querySelector('.rate-us')
 let ratingBtns = document.querySelectorAll("li")
 let messageText = document.querySelector('.message')
 let errorText = document.querySelector(".error")
-// buttons.forEach(button => {
-//     button.addEventListener('click', function () {
-//         buttons.forEach(btn => btn.classList.remove('active'));
-//         this.classList.add('active');  
-//     })      
-//     });
+
+
 let rating = 0;
 ratingBtns.forEach( ratingBtn => {
    ratingBtn.addEventListener('click', function () {
@@ -41,38 +37,4 @@ back.addEventListener('click', ()=>{
     nextPage.style.display = 'none';
 })
 
-
-// const cardOne = document.querySelector(".card-one");
-// const cardTwo = document.querySelector(".card-two");
-// const submitBtn = document.querySelector(".submit button");
-// const rattingBtns = document.querySelectorAll("li");
-// let error = document.querySelector(".error");
-// let ratting = 0;
-// const selectedRatting = document.querySelector(".selected-ratting");
-
-// rattingBtns.forEach(rattingBtn => {
-//   rattingBtn.addEventListener("click", () => {
-//     error.classList.add("hidden");
-//     removeActive();
-//     rattingBtn.classList.add("active");
-//     ratting = rattingBtn.dataset.value;
-//   })
-// })
-
-// submitBtn.addEventListener("click", () => {
-//   if (ratting === 0) {
-//     error.classList.remove("hidden");
-//   }
-//   else {
-//     selectedRatting.innerHTML = `You selected ${ratting} out of 5`;
-//     cardOne.classList.add("hidden");
-//     cardTwo.classList.remove("hidden");
-//   }
-// })
-
-// function removeActive() {
-//   for (const rattingBtn of rattingBtns) {
-//     rattingBtn.classList.remove("active");
-//   }
-// }
 
